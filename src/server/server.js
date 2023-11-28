ee61f13078a45c1c72d261f4c92dfb96468c86a3
medcg <martindcg88@gmail.com>
2023-11-28 12:24:19 -0300

    cambios

--- src/server/server.js
+++ src/server/server.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import cors from 'cors';
 import searchRoutes from '../routes/searchRoutes.js';
+import repoRoutes from '../routes/repoRoutes.js';
 
 
 const app = express ();
@@ -10,7 +11,7 @@ app.use(express.json());
 app.use(cors());
 
 
-app.use('/api', searchRoutes);
+app.use('/api', searchRoutes, repoRoutes);
 
 
 
